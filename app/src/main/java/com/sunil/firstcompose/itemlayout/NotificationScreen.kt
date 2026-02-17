82d2335a6e95c26b35c9abb51c52089a3ed7647d
sunilkrsharma1296 <46995568+sunilkrsharma1296@users.noreply.github.com>
2026-02-18 00:59:28 +0530

    first--->6

--- app/src/main/java/com/sunil/firstcompose/itemlayout/NotificationScreen.kt
+++ app/src/main/java/com/sunil/firstcompose/itemlayout/NotificationScreen.kt
@@ -1,32 +1,44 @@
 package com.sunil.firstcompose.itemlayout
 
+import android.media.MediaPlayer
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.sunil.firstcompose.R
 import com.sunil.firstcompose.ui.theme.BlogAppTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -41,6 +53,9 @@ fun NotificationScreen(){
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize(1f)
     ) {
+        Derived()
+        Loader()
+        MediaComposable()
         CoroutineScopeComposable()
         Spacer(modifier = Modifier.padding(10.dp))
         ListComposable()
@@ -173,5 +188,82 @@ fun Counter(value: Int) {
     Text(text = "Counter is $value")
 }
 
+@Composable
+fun MediaComposable() {
+    val context = LocalContext.current
 
+    DisposableEffect(key1 = Unit) {
+        val mediaPlayer= MediaPlayer.create(context, R.raw.ishq_song)
+        mediaPlayer.start()
+
+        Log.e("coroutineLaunch","Song Started...")
+
+        onDispose {
+            Log.e("coroutineLaunch","Song Stopped...")
+
+            mediaPlayer.stop()
+            mediaPlayer.release()
+        }
+
+    }
+
+}
+
+
+@Composable
+fun Loader() {
+    val degree= produceState(initialValue = 0) {
+        while (true) {
+            delay(16)
+            value = (value+10) % 360
+        }
+    }
+
+
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize(1f),
+        content = {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(imageVector = Icons.Outlined.Refresh,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(60.dp)
+                        .rotate(degree.value.toFloat())
+                )
+
+                Text("Loading...")
+            }
+        })
+}
+
+@Composable
+fun Derived() {
+    val tableOf = remember { mutableStateOf(5) }
+
+    val index = produceState(1) {
+        repeat(9){
+            delay(1000)
+            value = it + 1
+
+        }
+    }
+
+    val message by remember {
+        derivedStateOf {
+            "${index.value} * ${tableOf.value} = ${index.value * tableOf.value}"
+        }
+    }
+
+    Box(contentAlignment = Alignment.Top as Alignment,
+        modifier = Modifier
+            .fillMaxSize(1f)
+    ){
+        Text(text = message,
+            style = MaterialTheme.typography.titleLarge)
+
+    }
+
+
+}
 
